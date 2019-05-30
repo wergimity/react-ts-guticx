775861719ef64b8a736197f3739fa550d05ea58c
wergimity <wergimity@gmail.com>
2019-05-30 23:25:38 +0300

    2 files modified, 2 added, 2 deleted

---
+++ app/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const App = () => {
+  return <div>
+    <div className='navbar'>
+      <div className='navbar-container'>
+        <div className='navbar-brand' children='Woo!'/>
+        <div className='navbar-block' children='Woo!'/>
+      </div>
+    </div>
+  </div>;
+}
--- index.tsx
+++ index.tsx
@@ -1,31 +1,12 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
-import './style.css';
+import './index.css';
+import { App } from './app/app'
+import { BrowserRouter } from 'react-router-dom';
 
-interface AppProps { }
-interface AppState {
-  name: string;
-}
-
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
